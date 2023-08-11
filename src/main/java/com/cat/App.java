5f5915f7b660a178b1bf027e39a9dce7eeb74f64
Catherine <sunny4evil@gmail.com>
2023-08-11 19:39:31 +0300

    add Logger to App class

--- src/main/java/com/cat/App.java
+++ src/main/java/com/cat/App.java
@@ -1,13 +1,19 @@
 package com.cat;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args ) {
+        logger.info("her sey iyi");
+        System.out.println("Hello World!");
     }
 }
