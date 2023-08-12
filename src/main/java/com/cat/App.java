20ab825a93579ffd5b5f6f352a006681f8221590
Catherine <sunny4evil@gmail.com>
2023-08-12 19:26:36 +0300

    that is all

--- src/main/java/com/cat/App.java
+++ src/main/java/com/cat/App.java
@@ -1,10 +1,7 @@
 package com.cat;
 
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
 import lombok.extern.slf4j.Slf4j;
-import sun.jvm.hotspot.debugger.ThreadContext;
 
 /**
  * Hello world!
@@ -13,10 +10,8 @@ import sun.jvm.hotspot.debugger.ThreadContext;
 @Slf4j
 public class App {
 
-    private static final Logger specialLogger = LoggerFactory.getLogger(App.class);
-
     public static void main( String[] args ) {
-        specialLogger.error("bir sey oldu");
+
         log.info("her sey iyi");
         System.out.println("Hello World!");
         log.trace("cok guzel");
