31273a998bed81a67352f3787c9530765b5c7735
Catherine <sunny4evil@gmail.com>
2023-09-02 18:38:18 +0300

    add Logger field to javaUtil logging class

--- src/main/java/com/cat/LogWithJavaUtil.java
+++ src/main/java/com/cat/LogWithJavaUtil.java
@@ -1,4 +1,11 @@
 package com.cat;
 
+import lombok.Getter;
+
+import java.util.logging.Logger;
+
 public class LogWithJavaUtil {
+    @Getter
+    Logger logger = Logger.getLogger(LogWithJavaUtil.class.getName());
+
 }
