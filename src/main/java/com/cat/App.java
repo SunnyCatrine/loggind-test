f41265cd0702d6207de8414e0c68a43fe625311a
Catherine <sunny4evil@gmail.com>
2023-09-02 18:42:55 +0300

    add info message to App main with JavaUtilLogging

--- src/main/java/com/cat/App.java
+++ src/main/java/com/cat/App.java
@@ -12,9 +12,12 @@ public class App {
 
     public static void main( String[] args ) {
 
-        log.info("her sey iyi");
-        System.out.println("Hello World!");
-        log.trace("cok guzel");
-        log.warn("dikkat ol");
+        LogWithJavaUtil logWithJavaUtil = new LogWithJavaUtil();
+        logWithJavaUtil.getLogger().info("kopek var");
+
+//        log.info("her sey iyi");
+//        System.out.println("Hello World!");
+//        log.trace("cok guzel");
+//        log.warn("dikkat ol");
     }
 }
