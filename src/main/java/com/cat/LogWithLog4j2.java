1fba8e2e7f7deb4371810338972805628f0353cb
Catherine <sunny4evil@gmail.com>
2023-09-04 17:14:27 +0300

    add LogWithLog4j2 class

---
+++ src/main/java/com/cat/LogWithLog4j2.java
@@ -0,0 +1,10 @@
+package com.cat;
+
+import lombok.Getter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogWithLog4j2 {
+    @Getter
+    private Logger logger = LogManager.getLogger(LogWithLog4j2.class.getName());
+}
